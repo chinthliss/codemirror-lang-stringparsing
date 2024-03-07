3ae4f9ca8a2a183a3e20174c12dc1a877f55410f
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2024-03-07 17:21:14 +0000

    Start adding if functionality

--- src/highlight.ts
+++ src/highlight.ts
@@ -2,7 +2,7 @@ import { styleTags, tags as t } from "@lezer/highlight";
 
 export const stringParsingHighlighting = styleTags({
     "parameter": t.number,
-    "lookup Or": t.literal,
+    "lookup logic Or": t.literal,
     "math": t.operator,
     "command": t.processingInstruction,
     "comment": t.comment

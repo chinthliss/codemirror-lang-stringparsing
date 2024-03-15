881f3311ccc20c8a70f27b218f461675a627b1d5
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2024-03-15 17:07:23 +0000

    Almost completely rewrite grammar
    Now with no shams at present

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,22 +1,22 @@
 import { styleTags, tags as t } from "@lezer/highlight";
 
 export const stringParsingHighlighting = styleTags({
-    "parameter": t.number,
-    "lookup logic Or": t.literal,
-    "math": t.operator,
+    "lookup": t.controlKeyword,
     "command": t.processingInstruction,
-    "comment": t.comment
+    "math": t.arithmeticOperator,
+    "comment": t.comment,
+    "parameter": t.number
 });
 
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
 const syntax_colors = syntaxHighlighting(
     HighlightStyle.define(
         [
-            { tag: t.processingInstruction, color: "#db47b8" },
-            { tag: t.literal, color: "#7b87b8" },
-            { tag: t.operator, color: "#ab87b8" },
+            { tag: t.controlKeyword, color: "#7b87b8" },
+            { tag: t.processingInstruction, color: "#fb87b8" },
+            { tag: t.arithmeticOperator, color: "#ab87b8" },
+            { tag: t.comment, color: "#585858" },
             { tag: t.number, color: "#0b67b8" },
-            { tag: t.comment, color: "#585858" }
         ],
         { all: { color: "#787878" } }
     )

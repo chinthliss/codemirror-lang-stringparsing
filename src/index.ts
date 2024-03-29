8791709fc8c15c472b8e30e003db4134fe570251
chinthliss <55845146+chinthliss@users.noreply.github.com>
2024-03-29 00:41:35 +0000

    Stop using example tag styles

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import {parser} from "./stringparsing.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
 
 export const stringParsingLanguage = LRLanguage.define({
     parser: parser.configure({
         props: [
             indentNodeProp.add({
-                Application: delimitedIndent({closing: ")", align: false})
+                Application: delimitedIndent({closing: "]", align: false})
             }),
             foldNodeProp.add({
                 Application: foldInside
-            }),
-            styleTags({
-                Identifier: t.variableName,
-                Boolean: t.bool,
-                String: t.string,
-                LineComment: t.lineComment,
-                "( )": t.paren
             })
         ]
     }),

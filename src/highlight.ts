f4956d4d2a77c61487b99ffcfd4635638e9958fb
chinthliss <55845146+chinthliss@users.noreply.github.com>
2024-04-03 22:12:10 +0100

    Separate lookup and control structure functions highlight-wise

--- src/highlight.ts
+++ src/highlight.ts
@@ -2,7 +2,7 @@ import { styleTags, tags as t } from "@lezer/highlight";
 
 export const stringParsingHighlighting = styleTags({
     "command/...": t.processingInstruction,
-    "lookup/...": t.controlKeyword,
+    "lookup/...": t.keyword,
     "control": t.controlKeyword,
     "math": t.arithmeticOperator,
     "comment": t.comment,
@@ -11,12 +11,17 @@ export const stringParsingHighlighting = styleTags({
     "comparison": t.compareOperator
 });
 
+/*
+ These styles don't actually do anything in the package and aren't exported.
+ They're to allow being lazy using the testing playground.
+ */
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
 const syntax_colors = syntaxHighlighting(
     HighlightStyle.define(
         [
-            { tag: t.controlKeyword, color: "#7b87b8" },
-            { tag: t.processingInstruction, color: "#fb87b8" },
+            { tag: t.keyword, color: "#7b87b8" },
+            { tag: t.controlKeyword, color: "#f8835c" },
+            { tag: t.processingInstruction, color: "#8b00ff" },
             { tag: t.arithmeticOperator, color: "#ab87b8" },
             { tag: t.comment, color: "#585858" },
             { tag: t.number, color: "#0b67b8" },
